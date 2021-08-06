b3f7488e5c8e49c7aed0df10ee1ec28f748c4ae5
四方 秀 <shu.42111227@gmail.com>
2021-08-06 20:55:08 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,25 @@
+/* eslint-disable  */
+
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: string = "hoge";
+
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+let tuple: [number, string] = [0, "A"];
+
+let any1: any = false;
+
+const funcA = () => {
+  const test = "TEST";
+};
+
+let null1: null = null;
+
+let undefined1: undefined = undefined;
+
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 1, name: "aaa" };
